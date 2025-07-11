8746e2703014e8671213c4408aefa36f39f31c54
dyxdevv <wilgtwilgt@gmail.com>
2025-07-11 14:16:37 +0700

    Delete TB.lua

--- TB.lua
+++
@@ -1,7 +0,0 @@
-game.Players.PlayerAdded:Connect(function(player)
-    player:Kick(string.upper(
-        "YOU WERE KICKED FROM THIS GAME: EXPLOITING IS A BANNABLE OFFENSE. "..
-        "THIS ACTION LOG HAS BEEN SUBMITTED TO ROBLOX.\n\n"..
-        "(ERROR CODE: 267)"
-    ))
-end)
